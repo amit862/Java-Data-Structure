08d1082befa957a4d44f365d73d3739d4866ac00
Amit Verma <v99amit@gmail.com>
2022-10-21 23:23:08 +0530

    Commit

---
+++ Backtracking/Suduko Solver/sudukoSolver.java
@@ -0,0 +1,93 @@
+/*Write a program to solve a Sudoku puzzle by filling the empty cells.
+
+A sudoku solution must satisfy all of the following rules:
+
+Each of the digits 1-9 must occur exactly once in each row.
+Each of the digits 1-9 must occur exactly once in each column.
+Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+The '.' character indicates empty cells.
+
+ 
+
+Example 1:
+
+
+Input: board = [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]
+Output: [["5","3","4","6","7","8","9","1","2"],["6","7","2","1","9","5","3","4","8"],["1","9","8","3","4","2","5","6","7"],["8","5","9","7","6","1","4","2","3"],["4","2","6","8","5","3","7","9","1"],["7","1","3","9","2","4","8","5","6"],["9","6","1","5","3","7","2","8","4"],["2","8","7","4","1","9","6","3","5"],["3","4","5","2","8","6","1","7","9"]]
+Explanation: The input board is shown above and the only valid solution is shown below:
+
+
+ 
+
+Constraints:
+
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit or '.'.
+It is guaranteed that the input board has only one solution. */
+
+class Solution {
+    public boolean isSafe(char[][] board, int row, int col, int number) {
+        // row & column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == (char) (number + '0')) {
+                return false;
+            }
+            if (board[row][i] == (char) (number + '0')) {
+                return false;
+            }
+        }
+        // grid
+        int sr = (row / 3) * 3;
+        int sc = (col / 3) * 3;
+
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == (char) (number + '0')) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean helper(char[][] board, int row, int col) {
+        if (row == board.length) {
+            return true;
+        }
+
+        int nrow = 0;
+        int ncol = 0;
+        if (col != board.length - 1) {
+            nrow = row;
+            ncol = col + 1;
+        } else {
+            nrow = row + 1;
+            ncol = 0;
+        }
+
+        if (board[row][col] != '.') {
+            if (helper(board, nrow, ncol)) {
+                return true;
+            }
+        } else {
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+                    board[row][col] = (char) (i + '0');
+                    if (helper(board, nrow, ncol)) {
+                        return true;
+                    } else {
+                        board[row][col] = '.';
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public void solveSudoku(char[][] board) {
+        helper(board, 0, 0);
+    }
+}
