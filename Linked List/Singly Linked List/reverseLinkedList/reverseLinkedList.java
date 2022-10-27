242a2fdad176c53082098f56c109426851f2de4a
Amit Verma <v99amit@gmail.com>
2022-10-28 00:14:52 +0530

    Create reverseLinkedList.java

---
+++ Linked List/Singly Linked List/reverseLinkedList/reverseLinkedList.java
@@ -0,0 +1,124 @@
+/*Reverse a linked list */
+/* Scratch Implementation of linked list*/
+
+public class reverseLinkedList {
+    Node head;
+    private int size;
+
+    reverseLinkedList() {
+        this.size = 0;
+    }
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+            size++;
+        }
+    }
+
+    // add - first, last
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // add - last
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    // print
+    public void printList() {
+        if (head == null) {
+            System.out.print("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + " -> ");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    // delete first
+    public void deleteFirst() {
+        if (head == null) {
+            System.out.println("The list is empty");
+            return;
+        }
+        size--;
+        head = head.next;
+    }
+
+    // delete last
+    public void deleteLast() {
+        if (head == null) {
+            System.out.println("The list is empty");
+            return;
+        }
+        size--;
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+
+        Node secondLast = head;
+        Node lastNode = head.next;
+        while (lastNode.next != null) {
+            lastNode = lastNode.next;
+            secondLast = secondLast.next;
+        }
+
+        secondLast.next = null;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+
+    public Node reverseRecursive(Node head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        Node newHead = reverseRecursive(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        reverseLinkedList list = new reverseLinkedList();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.printList();
+
+        list.head = list.reverseRecursive(list.head);
+        list.printList();
+
+    }
+}
