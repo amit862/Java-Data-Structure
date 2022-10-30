42d67cb3c1b8b7827531d2a7395c08d6b6db5737
Amit Verma <v99amit@gmail.com>
2022-10-31 00:41:49 +0530

    Commit

---
+++ Linked List/Singly Linked List/palindromeLinkedList.java
@@ -0,0 +1,81 @@
+/* Palindrome Linked List 
+ * Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    public ListNode findMiddle(ListNode head) {
+        ListNode hare = head;
+        ListNode turtle = head;
+
+        while (hare.next != null && hare.next.next != null) {
+            hare = hare.next.next;
+            turtle = turtle.next;
+        }
+
+        return turtle;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+
+        ListNode middle = findMiddle(head); // 1st half end
+        ListNode secondHalfStart = reverse(middle.next);
+
+        ListNode firstHalfStart = head;
+        while (secondHalfStart != null) {
+            if (firstHalfStart.val != secondHalfStart.val) {
+                return false;
+            }
+            firstHalfStart = firstHalfStart.next;
+            secondHalfStart = secondHalfStart.next;
+        }
+
+        return true;
+    }
+}
