1feac09d942fa98fe72d8b49bcb780cac4fbde23
Amit Verma <v99amit@gmail.com>
2022-11-01 01:00:09 +0530

    Commit

---
+++ Stack/Stack_Class.java
@@ -0,0 +1,50 @@
+/*Implementation of stack using ArrayList */
+
+import java.util.ArrayList;
+
+public class Stack_Class {
+    static class Stack {
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        public static boolean isEmpty() {
+            return list.size() == 0;
+        }
+
+        // push
+        public static void push(int data) {
+            list.add(data);
+        }
+
+        // pop
+        public static int pop() {
+            if (isEmpty()) {
+                return -1;
+            }
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        // peek
+        public static int peek() {
+            if (isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
