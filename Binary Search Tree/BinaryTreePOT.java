e5032f7eb1bd0e3d7436b92a1bf4a130008c0dc4
Amit Verma <v99amit@gmail.com>
2022-11-03 00:06:20 +0530

    Commit

---
+++ Binary Search Tree/BinaryTreePOT.java
@@ -0,0 +1,52 @@
+/* Post-order Traversal...... 
+Rule:- (1) Left Subtree
+       (2) Right Subtree
+       (3) Root
+*/
+
+public class BinaryTreePOT {  //T.C - O(n)
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;
+
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void postorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+        
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        postorder(root);
+    }
+}
