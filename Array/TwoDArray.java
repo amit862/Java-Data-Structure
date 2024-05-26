635558b08103139fb554e0930fba996c76e8cf4d
Amit Verma <77237968+amit862@users.noreply.github.com>
2024-05-26 22:56:46 +0530

    Update TwoDArray.java

--- Array/TwoDArray.java
+++ Array/TwoDArray.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class TwoDArray {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
         int cols = sc.nextInt();
