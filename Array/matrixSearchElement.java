bfbd2577e593919a8f5253671c3e7e89f0af1f7b
Amit Verma <77237968+amit862@users.noreply.github.com>
2024-06-01 21:06:55 +0530

    Update matrixSearchElement.java

--- Array/matrixSearchElement.java
+++ Array/matrixSearchElement.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class matrixSearchElement {
+    
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int rows = sc.nextInt();
